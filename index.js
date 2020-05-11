47d4764073254f04e3903325d4fb875b6c9e9f98
Gordon Taylor <ivyterrace@hotmail.co.uk>
2020-05-11 10:21:28 +0100

    gpackage.json and index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const geoFunctions = require('./functions.js');
+const Point = require('./class-point.js').Point;
+const Path = require('../src/class-path').Path;
+
+module.exports = {
+  geoFunctions,
+  Point,
+  Path
+}
